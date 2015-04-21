cae3cb7428aa36d25cc51f7c03f4760af2955ee3
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 15:13:39 +0800

    Create 14151188_郝策_5.9.c

---
+++ 14151188_郝策_5.9.c
@@ -0,0 +1,24 @@
+#include"stdio.h"
+int main()
+{
+	int n,i,s;
+	for(n=2;n<=1000;n++)
+	{
+		s=0;
+		for(i=1;i<n;i++)
+		{
+			if((n%i)==0)
+			s=s+i;
+		}
+		if(s==n)
+		{
+			printf("%d,its factors are ",n);
+			for(i=1;i<n;i++)
+			{
+				if((n%i)==0)
+				printf("%d ",i);
+			}
+			printf("\n");
+		}
+	}
+}
