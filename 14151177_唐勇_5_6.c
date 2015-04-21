b73bdf19fe07af531097674fda2776e82be6bb30
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 14:18:45 +0800

    Create 14151177_唐勇_5_6.c

---
+++ 14151177_唐勇_5_6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int sum=0,n=1;
+	int i,j;
+	for(j=1;j<=5;j++)
+	{
+		for(i=1;i<=j;i++)
+		{n=n*i;
+		}
+		sum=sum+n;
+	    printf("%d!=%d\n",j,n);
+	    n=1;
+	}
+	printf("%d",sum);
+}
