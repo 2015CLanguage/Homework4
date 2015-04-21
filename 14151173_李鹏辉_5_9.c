a89a029fe8cde5745a491d551a118d964ffa473a
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 23:00:59 +0800

    Create 14151173_李鹏辉_5_9.c

---
+++ 14151173_李鹏辉_5_9.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	int s,i,j;
+	for(i=2;i<=1000;i++)
+	{
+		s=1;
+		for(j=2;j<=i-1;j++)
+		{
+			if(i%j==0) 
+				s=j+s;
+		}
+		if(i==s)
+		{
+			printf("%d ",i);
+		    printf("its factaors are 1");
+		    for(j=2;j<=i-1;j++)
+			{
+			    if(i%j==0) 
+				    printf(",%d",j);
+			}
+			printf("\n");
+		}
+	}
+}
