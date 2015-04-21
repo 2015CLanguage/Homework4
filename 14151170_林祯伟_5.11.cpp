ec3e88ceeec8085bed853dbce2a452ba65e1887e
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 21:03:31 +0800

    Create 14151170_林祯伟_5.11.cpp

---
+++ 14151170_林祯伟_5.11.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int i,j;
+	float h[10],s;
+	h[0]=100.0;
+	
+	for(i=1;i<=9;i++)
+	{
+		h[i]=h[i-1]/2;
+	}
+	for(j=0;j<=9;j++)
+	{
+		s=s+h[j];
+	}
+	printf("第十次落地时总共经过%f米。\n",s);
+	printf("第十次落地时球的高度%f米。\n",h[9]);
+	
+	return 0;
+ } 
