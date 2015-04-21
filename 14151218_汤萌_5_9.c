750a76b4fd664983319ba3139dea922e6799d8fc
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 15:00:09 +0800

    Create 14151218_汤萌_5_9.c

---
+++ 14151218_汤萌_5_9.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, i, m;
+	for (i=2; i<1001; i++)
+	{
+		a=0;
+		for (m=1; m<i; m++)
+		{
+			if (i%m==0)
+				a=a+m;
+		}
+		if (a==i)
+		{
+			printf("%d its factors are 1", i);
+			for (m=2; m<i+1; m++)
+			{
+				if (i%m==0)
+				{
+					printf(", %d", m);
+				}
+			}
+			printf("\n");
+		}
+	}
+return 0;
+}
