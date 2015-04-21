125f2472708f2e6d5372855e062c97074c20b4ce
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 22:06:20 +0800

    Create 14151172_程涛_5_11.c

---
+++ 14151172_程涛_5_11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+	double n=100,p,q=100;
+	int i,t=0;
+	p=1;
+	for(i=1;i<10;i++)
+	{	
+		p=p*0.5;
+		q=q+2*n*p;
+	}	
+	p=n;
+	for(i=1;i<=10;i++)
+	{
+		p=p*0.5;
+	}
+	printf("第10次落地时，共经过了%lf米\n",q);
+	printf("第10次反弹高度为%lf米\n",p);
+}
