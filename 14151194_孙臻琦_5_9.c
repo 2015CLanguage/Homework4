abf3cbf997da2b6b70ea19108339c07a360f010b
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 14:28:45 +0800

    Create 14151194_孙臻琦_5_9.c

---
+++ 14151194_孙臻琦_5_9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n,s,i;
+    for (n=2;n<=1000;n++)
+	{
+		s=0;
+        for(i=1;i<n;i++)
+        if((n%i)==0)
+			s=s+i;
+        if(s==n)
+		{
+			printf("%d,its factors are:", n);
+            for(i=1;i<n;i++)
+            if (n%i==0) printf("%d ",i);
+            printf("\n");
+		}
+	}
+    return 0;
+}
