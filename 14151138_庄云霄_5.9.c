d0fd6fc4b2558e776a3c11295d18c9fe290050a0
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 14:59:32 +0800

    Create 14151138_庄云霄_5.9.c

---
+++ 14151138_庄云霄_5.9.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int i,j,a;
+
+	for(j=2;j<1000;j++){
+		a=0;
+		for(i=1;i<j;i++)
+			if((j%i)==0) a=a+i;
+		
+		if(a==j)
+		{
+			printf("%d,Its factors are :",j);
+			for(i=1;i<=j;i++)
+				if(j%i==0) printf("%d,",i);
+			printf("\n");
+		}
+	}
+
+
+
+
+
+}
