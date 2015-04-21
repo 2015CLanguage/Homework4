08a41a6d36d295105886c3c8dd326844fb5c6c98
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 17:26:37 +0800

    Create 14151124_孙宇童_5.9.C

---
+++ 14151124_孙宇童_5.9.C
@@ -0,0 +1,31 @@
+int main(void)
+{
+	int i,j,sum;
+	
+	for(i=1;i<=1000;i++)//用for循环一个一个试验1000以内的数
+	{
+		for(j=1,sum=0;j<i;j++)//看其因子相加之和
+		{
+			if(i%j==0)
+				sum=sum+j;
+			
+		}
+			if(sum==i)//判断和与该数是否相等
+			{
+				printf("%d its factors are",sum);
+				for(j=1;j<i;j++)//将因数输出
+				{
+					if(sum%j==0)
+					{
+						printf("%d ",j);
+					}
+			
+				}
+				printf("\n");
+
+			}
+			
+		
+	}
+	return 0;
+}
