f2b8589bdbd574ce5c8dda5fe284ab6db4cb567f
2015CLanguage <buaa_2015_c@163.com>
2015-04-21 22:55:21 +0800

    Create 14151173_李鹏辉_5_3.c

---
+++ 14151173_李鹏辉_5_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int m,n,a,b;
+	scanf("%d %d",&m,&n);
+    a=m*n;
+	while(m!=n)
+	{
+		if(m>n) m=m-n;
+		else n=n-m;
+	}
+	b=a/m;
+	printf("a=%d,b=%d\n",m,b);   //a为最大公约数，b为最小公倍数
+}
