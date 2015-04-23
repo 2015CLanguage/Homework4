9deb05075183cfb2445619add315d01242438a5c
2015CLanguage <buaa_2015_c@163.com>
2015-04-23 18:34:07 +0800

    Update 14151171_李资晨_5_3.c

--- 14151171_李资晨_5_3.c
+++ 14151171_李资晨_5_3.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int sum=0,j=1,n;
-	int i;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		j=j*i;
-		sum=sum+j;
-
-	}
-	printf("sum=%d\n",sum);
+	int m, n,t,i;
+	scanf("%d%d", &m, &n);
+	if (m < n)t = m;
+	else t = n;
+	while (m%t != 0 || n%t != 0)
+		t = t - 1;
+	i = m*n / t;
+	printf("最大公约数=%d\n最小公倍数=%d\n", t,i);
 	return 0;
+
 }
