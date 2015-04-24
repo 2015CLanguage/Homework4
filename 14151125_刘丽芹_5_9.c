3fb364c99048a4b661aae8ce4232da41f2121961
2015CLanguage <buaa_2015_c@163.com>
2015-04-24 13:22:22 +0800

    Create 14151125_刘丽芹_5_9.c

---
+++ 14151125_刘丽芹_5_9.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,m,i,j;
+    for (n=2;n<1000;n++)
+    {
+        m=0;
+        for (i=1;i<n;i++)
+        {
+            if ((n%i)==0)
+            m=m+i;
+        }
+        if (m==n)
+        {
+            printf ("%d,its factors are",n);
+            for (j=1;j<1000;j++)
+                if((m%j)==0)
+                   printf("%d ",j);
+            printf("\n");
+        }
+
+
+    }
+    return 0;
+}
