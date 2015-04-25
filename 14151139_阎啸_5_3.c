38fa559448e52e0ffff1b5d60e2b926073b4e1d4
2015CLanguage <buaa_2015_c@163.com>
2015-04-25 22:36:23 +0800

    Create 14151139_阎啸_5_3.c

---
+++ 14151139_阎啸_5_3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,n,m;
+	scanf("%d,%d",&n,&m);
+	if(m<n)
+	{
+		a=m;
+		m=n;
+		n=a;
+	}
+	c=n*m;
+	while(n!=0)
+        {	
+	 b=m%n;
+	 m=n;
+	 n=b;
+	}
+	printf("最大公约数%d\n最小公倍数%d\n",m,c/m);
+}
