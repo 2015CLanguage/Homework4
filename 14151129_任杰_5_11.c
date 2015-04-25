58d090160a0ba3076c3cf2f74010ef9003f902e7
2015CLanguage <buaa_2015_c@163.com>
2015-04-25 22:19:02 +0800

    Create 14151129_任杰_5_11.c

---
+++ 14151129_任杰_5_11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	float a, b,sum;
+	b = 100;
+	sum = 0;
+	for (a = 1; a < 10; a++)
+	{
+		sum = sum + 1.5*b;
+		b = b / 2;
+	}
+	printf("%f\n", sum);
+	printf("%f\n", b/2);
+}
