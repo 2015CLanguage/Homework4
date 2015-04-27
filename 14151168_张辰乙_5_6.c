ded072977ab4d100466bb033f4aa02ecddcca2d1
2015CLanguage <buaa_2015_c@163.com>
2015-04-27 22:57:06 +0800

    Create 14151168_张辰乙_5_6.c

---
+++ 14151168_张辰乙_5_6.c
@@ -0,0 +1,16 @@
+ 	    #include"stdio.h" 
+ int main() 
+ { 
+ 	      int a,b,c=0,i; 
+ 	      for(a=1;a<=5;a++) 
+ 	     { 
+ 		          b=1; 
+ 		          for(i=1;i<=a;i++) 
+ 		         { 
+ 			                b=b*i; 
+ 		         } 
+ 		         c=c+b; 
+ 	     } 
+ printf("%d",c); 
+ return 0; 
+ } 
