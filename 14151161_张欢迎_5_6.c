e40f8166cb5cbf5962be8652343eb113d6ed63f4
2015CLanguage <buaa_2015_c@163.com>
2015-05-05 23:20:30 +0800

    Create 14151161_张欢迎_5_6.c

---
+++ 14151161_张欢迎_5_6.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main() 
+{	int n,m;//n计数，m接收输入 
+	double s=0,t=1; 
+	printf("请输入正整数m:"); 
+	scanf("%d",&m); 
+	for (n=1;n<=m;n++)//t存放累乘，s存放和 
+		{ 
+			t=t*n; 
+			s=s+t; 
+
+ 
+		} 
+ 	printf("1!+2!+...+%d!=%22.15e\n",m,s); 
+ 	return 0; 
+ } 
